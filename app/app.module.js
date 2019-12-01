18d6c006e8dc70d491cecdd5ec6209cd022a16dd
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-01 19:14:51 +0700

    nothing

--- app/app.module.js
+++ app/app.module.js
@@ -37,56 +37,56 @@ app
     .component("appDangNhap", DangNhapComponent)
     ;
 
-/* Services */    
-app.service('anchorSmoothScroll', function() {
+/* Services */
+app.service('anchorSmoothScroll', function () {
     this.scrollTo = function (eID) {
+        angular.element(document).ready(() => {
 
-        // This scrolling function 
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+            // This scrolling function 
+            // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
 
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY); return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for (var i = startY; i < stopY; i += step) {
+            let startY = currentYPosition();
+            let stopY = elmYPosition(eID);
+            let distance = stopY > startY ? stopY - startY : startY - stopY;
+            if (distance < 100) {
+                scrollTo(0, stopY); return;
+            }
+            let speed = Math.round(distance / 100);
+            if (speed >= 20) speed = 20;
+            let step = Math.round(distance / 25);
+            let leapY = stopY > startY ? startY + step : startY - step;
+            let timer = 0;
+            if (stopY > startY) {
+                for (let i = startY; i < stopY; i += step) {
+                    setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+                    leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+                } return;
+            }
+            for (let i = startY; i > stopY; i -= step) {
                 setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
-        }
-        for (var i = startY; i > stopY; i -= step) {
-            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-        }
+                leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+            }
 
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
+            function currentYPosition() {
+                // Firefox, Chrome, Opera, Safari
+                if (self.pageYOffset) return self.pageYOffset;
+                // Internet Explorer 6 - standards mode
+                if (document.documentElement && document.documentElement.scrollTop)
+                    return document.documentElement.scrollTop;
+                // Internet Explorer 6, 7 and 8
+                if (document.body.scrollTop) return document.body.scrollTop;
+                return 0;
+            }
 
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
-
+            function elmYPosition(eID) {
+                let elm = document.getElementById(eID);
+                let y = elm.offsetTop;
+                let node = elm;
+                while (node.offsetParent && node.offsetParent != document.body) {
+                    node = node.offsetParent;
+                    y += node.offsetTop;
+                } return y;
+            }
+        });
     };
-
 });
