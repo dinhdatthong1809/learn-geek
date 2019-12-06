e2d13b51f0ef616d6cb909b18df3cc7cb1e9235d
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-07 00:45:04 +0700

    [nothing]

---
+++ assets/js/init-firebase.js
@@ -0,0 +1,15 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyA_ToXe8aeSzN9c92s0idgKkgCzHbmYrTc",
+    authDomain: "learngeek-f085b.firebaseapp.com",
+    databaseURL: "https://learngeek-f085b.firebaseio.com",
+    projectId: "learngeek-f085b",
+    storageBucket: "learngeek-f085b.appspot.com",
+    messagingSenderId: "600989818649",
+    appId: "1:600989818649:web:3f14a9380cd4a7f0d82f88",
+    measurementId: "G-5SJ439C7CX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export let learnGeekDB = firebase.firestore();
