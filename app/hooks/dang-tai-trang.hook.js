e23325c6ab54baf99b3ca819b359d16589963f1d
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-08 11:57:59 +0700

    [nothing]

--- app/hooks/dang-tai-trang.hook.js
+++ app/hooks/dang-tai-trang.hook.js
@@ -1,16 +1,16 @@
 // Hook dùng để hiện dấu hiệu đang tải trang trong khi chuyển trang
 export let loadingIndicatorHook = ($transitions, $document) => {
-    let body = $document.find("body");
+  let body = $document.find("body");
 
-    let showLoadingIndicator = () => {
-      body.append(angular.element('<div class="text-secondary" id="spinner"><i class="fas fa-circle-notch fa-10x fa-spin" aria-hidden="true"></i></div>'));
-    };
+  let showLoadingIndicator = () => {
+    body.append(angular.element('<div class="text-secondary" id="spinner"><i class="fas fa-circle-notch fa-10x fa-spin" aria-hidden="true"></i></div>'));
+  };
 
-    let hideLoadingIndicator = () => {
-      let spinner = document.getElementById("spinner");
-      spinner.parentElement.removeChild(spinner);
-    };
-    
-    $transitions.onStart({}, showLoadingIndicator);
-    $transitions.onFinish({}, hideLoadingIndicator);
+  let hideLoadingIndicator = () => {
+    let spinner = document.getElementById("spinner");
+    spinner.parentElement.removeChild(spinner);
+  };
+
+  $transitions.onStart({}, showLoadingIndicator);
+  $transitions.onFinish({}, hideLoadingIndicator);
 }
