3a167446ac3dd8b42bf22770dfed208337521a85
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-08 21:28:53 +0700

    [nothing]

--- app/modules/account-module/account-form/account-form.component.js
+++ app/modules/account-module/account-form/account-form.component.js
@@ -14,62 +14,74 @@ function controller($window, accountService, authenticationService) {
     this.signUp = () => {
         SweetAlertHelper.choXuLy();
 
-        // Thử đăng kí tài khoản mới
-        authenticationService
-            .signUp(this.account.email, this.account.password)
+        // kiếm tra tên đăng nhập đã tồn tại hay chưa
+        accountService
+            .getOne(this.account.username)
             .then(
-                // nếu đăng ký thành công...
-                (result) => {
-                    // thì thêm dữ liệu vào database
-                    accountService
-                        .insert(this.account)
+                (doc) => {
+                    if (doc.exists) {
+                        SweetAlertHelper.thatBai("Tên đăng nhập đã tồn tại!");
+                        return;
+                    }
+
+                    // Thử đăng kí tài khoản mới
+                    authenticationService
+                        .signUp(this.account.email, this.account.password)
                         .then(
-                            // nếu thêm dữ liệu thành công...
-                            () => {
-                                // thì cập nhật displayName trong tài khoản
-                                result.user.updateProfile(
-                                    {
-                                        displayName: this.account.username
-                                    }
-                                )
-                                .then(
-                                    // nếu cập nhật displayName thành công...
-                                    () => {
-                                        // thì xuất thông báo và về trang chủ
-                                        SweetAlertHelper
-                                            .thanhCong("Đăng ký thành công!")
-                                            .then(
-                                                (result) => {
-                                                    if (result.value) {
-                                                        $window.location = '/';
-                                                    };
+                            // nếu đăng ký thành công...
+                            (result) => {
+                                // thì thêm dữ liệu vào database
+                                accountService
+                                    .insert(this.account)
+                                    .then(
+                                        // nếu thêm dữ liệu thành công...
+                                        () => {
+                                            // thì cập nhật displayName trong tài khoản
+                                            result.user.updateProfile(
+                                                {
+                                                    displayName: this.account.username
                                                 }
-                                            );
-                                    }
-                                );
+                                            )
+                                                .then(
+                                                    // nếu cập nhật displayName thành công...
+                                                    () => {
+                                                        // thì xuất thông báo và về trang chủ
+                                                        SweetAlertHelper
+                                                            .thanhCong("Đăng ký thành công!")
+                                                            .then(
+                                                                (result) => {
+                                                                    if (result.value) {
+                                                                        $window.location = '/';
+                                                                    };
+                                                                }
+                                                            );
+                                                    }
+                                                );
+                                        }
+                                    );
+                            }
+                        )
+                        .catch(
+                            (error) => {
+                                switch (error.code) {
+                                    case 'auth/email-already-in-use':
+                                        SweetAlertHelper.thatBai("Email này đã được sử dụng. Vui lòng chọn email khác.");
+                                        break;
+                                    case 'auth/invalid-email':
+                                        SweetAlertHelper.thatBai("Email không hợp lệ.");
+                                        break;
+                                    case 'auth/operation-not-allowed':
+                                        SweetAlertHelper.thatBai("Có lỗi khi đăng ký.");
+                                        break;
+                                    case 'auth/weak-password':
+                                        SweetAlertHelper.thatBai("Mật khẩu chưa đủ mạnh. Vui lòng thêm cả chữ lẫn số vào mật khẩu.");
+                                        break;
+                                    default:
+                                        SweetAlertHelper.thatBai("Đăng ký thất bại!");
+                                        console.log(error);
+                                }
                             }
                         );
-                }
-            )
-            .catch(
-                (error) => {
-                    switch (error.code) {
-                        case 'auth/email-already-in-use':
-                            SweetAlertHelper.thatBai("Email này đã được sử dụng. Vui lòng chọn email khác.");
-                            break;
-                        case 'auth/invalid-email':
-                            SweetAlertHelper.thatBai("Email không hợp lệ.");
-                            break;
-                        case 'auth/operation-not-allowed':
-                            SweetAlertHelper.thatBai("Có lỗi khi đăng ký.");
-                            break;
-                        case 'auth/weak-password':
-                            SweetAlertHelper.thatBai("Mật khẩu chưa đủ mạnh. Vui lòng thêm cả chữ lẫn số vào mật khẩu.");
-                            break;
-                        default:
-                            SweetAlertHelper.thatBai("Đăng ký thất bại!");
-                            console.log(error);
-                    }
                 }
             );
     }
