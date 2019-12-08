06b34428b5304ebf20f90463562f25f1601574c6
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-09 00:33:49 +0700

    [nothing]

--- app/xac-thuc/quen-mat-khau/quen-mat-khau.component.js
+++ app/xac-thuc/quen-mat-khau/quen-mat-khau.component.js
@@ -1,9 +1,34 @@
+import SweetAlertHelper from '../../../assets/js/sweet-alert-helper.js';
+
 export let QuenMatKhauComponent = {
     templateUrl: './app/xac-thuc/quen-mat-khau/quen-mat-khau.component.html',
     controller: controller,
     controllerAs: '$scope'
 };
 
-function controller() {
+function controller(authenticationService) {
     this.email = "";
+
+    this.sendPasswordResetEmail = () => {
+        SweetAlertHelper.choXuLy();
+
+        var actionCodeSettings = {
+            url: 'https://learngeek.netlify.com/',
+            handleCodeInApp: true
+        };
+
+        authenticationService
+            .sendPasswordResetEmail(this.email, actionCodeSettings)
+            .then(
+                () => {
+                    SweetAlertHelper.thanhCong("Gửi email thành công!");
+                }
+            )
+            .catch(
+                (error) => {
+                    SweetAlertHelper.thatBai("Gửi email thât bại!");
+                    console.log(error);
+                }
+            );
+    }
 }
--- assets/js/sweet-alert-helper.js
+++ assets/js/sweet-alert-helper.js
@@ -3,57 +3,60 @@ import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@9.3.17/src/sweetalert
 export default class SweetAlertHelper {
 
 	static thanhCong = (thongBao) => {
-    	return Swal.fire({
-			icon : 'success',
-			title : 'Thành công',
-			html : thongBao,
-			showConfirmButton : true
+		return Swal.fire({
+			icon: 'success',
+			title: 'Thành công',
+			html: thongBao,
+			showConfirmButton: true
 		});
-    };
-    
-    static thatBai = (thongBao) => {
-    	return Swal.fire({
-			icon : 'error',
-			title : 'Lỗi',
-			html : thongBao,
-			showConfirmButton : true
+	};
+
+	static thatBai = (thongBao) => {
+		return Swal.fire({
+			icon: 'error',
+			title: 'Lỗi',
+			html: thongBao,
+			showConfirmButton: true
 		});
-    }
-    
-    static canhBao = (thongBao) => {
-    	return Swal.fire({
-			icon : 'warning',
-			title : 'Cảnh báo',
-			html : thongBao,
-			showConfirmButton : true
+	}
+
+	static canhBao = (thongBao) => {
+		return Swal.fire({
+			icon: 'warning',
+			title: 'Cảnh báo',
+			html: thongBao,
+			showConfirmButton: true
 		});
-    }
-    
-    static hoi = (thongBao, hamDongY) => {
-    	return Swal.fire({
-    		icon: 'warning',
-    		title: 'Bạn chắc chắn chứ?',
-    		html: thongBao,
-    		showCancelButton: true,
-    		focusCancel: true,
-    		cancelButtonText: 'Hủy!',
-    		cancelButtonColor: '#d33',
-    		confirmButtonText: 'Đồng ý!',
-    		confirmButtonColor: '#3085d6'
-    	}).then((result) => {
-    		if (result.value) {
-    			hamDongY();
-    		}
-    	});
-    }
-    
-    static choXuLy = () => {
-    	return Swal.fire({
-    		title: "Đang xử lý",
-    		html: "Vui lòng chờ kết quả",
-    		showConfirmButton: false,
-    		allowOutsideClick: false
-    	});
-    }
-    
+	}
+
+	static hoi = (thongBao, hamDongY) => {
+		return Swal.fire({
+			icon: 'warning',
+			title: 'Bạn chắc chắn chứ?',
+			html: thongBao,
+			showCancelButton: true,
+			focusCancel: true,
+			cancelButtonText: 'Hủy!',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Đồng ý!',
+			confirmButtonColor: '#3085d6'
+		}).then((result) => {
+			if (result.value) {
+				hamDongY();
+			}
+		});
+	}
+
+	static choXuLy = () => {
+		return Swal.fire({
+			title: "Đang xử lý",
+			html: "Vui lòng chờ kết quả",
+			showConfirmButton: false,
+			allowOutsideClick: false,
+			onBeforeOpen: () => {
+				Swal.showLoading();
+			}
+		});
+	}
+
 }
