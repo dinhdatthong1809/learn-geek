60068e93dd0e8a23d0d46329450eddd5b02b4cd2
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-08 11:46:14 +0700

    [nothing]

--- app/hooks/dang-tai-trang.hook.js
+++ app/hooks/dang-tai-trang.hook.js
@@ -3,14 +3,14 @@ export let loadingIndicatorHook = ($transitions, $document) => {
     let body = $document.find("body");
 
     let showLoadingIndicator = () => {
-      body.append(angular.element('<div id="spinner"><i class="fa fa-spinner fa-pulse fa-3x fa-fw" aria-hidden="true"></i>heheehe</div>'));
+      body.append(angular.element('<div class="text-secondary" id="spinner"><i class="fas fa-circle-notch fa-10x" aria-hidden="true"></i></div>'));
     };
 
     let hideLoadingIndicator = () => {
       let spinner = document.getElementById("spinner");
       spinner.parentElement.removeChild(spinner);
     };
-
+    
     $transitions.onStart({}, showLoadingIndicator);
     $transitions.onFinish({}, hideLoadingIndicator);
 }
