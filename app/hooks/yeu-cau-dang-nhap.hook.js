85fd9428e4b86bafd35a79dbcc619440007eb630
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-10 01:24:23 +0700

    [nothing]

--- app/hooks/yeu-cau-dang-nhap.hook.js
+++ app/hooks/yeu-cau-dang-nhap.hook.js
@@ -1,15 +1,23 @@
+import { learnGeekAuth } from '../../assets/js/init-firebase.js';
+
 // hook yêu cầu đăng nhập trước khi vào một trang cần xác thực
-export let requireAuthHook = ($transitions, authenticationService) => {
-    
+export let requireAuthHook = ($transitions) => {
+
     let requiresAuthCriteria = {
         to: (state) => state.data && state.data.requiresAuth
     };
-    
-    $transitions.onBefore(requiresAuthCriteria, (trans) => {
-        let currentUser = authenticationService.isAuthenticated();
 
-        if (currentUser === null) {
-            return trans.router.stateService.target('dang-nhap');
-        }
+    $transitions.onBefore(requiresAuthCriteria, (trans) => {
+        learnGeekAuth.onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+            } else {
+                trans.router.stateService.target('dang-nhap');
+            }
+        });
+
+        // if (currentUser === null) {
+        //     return 
+        // }
     });
 }
