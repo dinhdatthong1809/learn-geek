b1bc78a54c9ce8fa7beba113c96a2b1154065750
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-09 20:17:29 +0700

    [nothing]

--- app/xac-thuc/quen-mat-khau/quen-mat-khau.component.js
+++ app/xac-thuc/quen-mat-khau/quen-mat-khau.component.js
@@ -21,8 +21,17 @@ function controller(authenticationService) {
             )
             .catch(
                 (error) => {
-                    SweetAlertHelper.thatBai("Gửi email thât bại!");
-                    console.log(error);
+                    switch (error.code) {
+                        case 'auth/invalid-email':
+                            SweetAlertHelper.thatBai("Email không hợp lệ.");
+                            break;
+                        case 'auth/user-not-found':
+                            SweetAlertHelper.thatBai("Không tồn tại tài khoản có email này.");
+                            break;
+                        default:
+                            SweetAlertHelper.thatBai("Gửi email thât bại!");
+                            console.log(error);
+                    }
                 }
             );
     }
