efbaea83960b4234763a6c2e5b2ce8a9b876a562
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-10 00:09:44 +0700

    [nothing]

--- app/hooks/yeu-cau-dang-nhap.hook.js
+++ app/hooks/yeu-cau-dang-nhap.hook.js
@@ -1,13 +1,11 @@
 // hook yêu cầu đăng nhập trước khi vào một trang cần xác thực
-export let requireAuthHook = ($transitions) => {
+export let requireAuthHook = ($transitions, authenticationService) => {
     
     let requiresAuthCriteria = {
         to: (state) => state.data && state.data.requiresAuth
     };
     
     $transitions.onBefore(requiresAuthCriteria, (trans) => {
-        let authenticationService = trans.injector().get('authenticationService');
-        
         let currentUser = authenticationService.isAuthenticated();
 
         if (currentUser === null) {
