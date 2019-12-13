0f5429692d87abe551e01f554b27f4904b0d576f
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-13 20:50:25 +0700

    [nothing]

--- app/hooks/dang-lam-bai.hook.js
+++ app/hooks/dang-lam-bai.hook.js
@@ -3,24 +3,27 @@ import SweetAlertHelper from '../../assets/js/sweet-alert-helper.js';
 // Hook dùng để báo hiệu nếu người dùng đang làm bài mà có ý định chuyển trang
 export let isDoingQuizHook = ($transitions) => {
 
-  let isDoingQuizIndicator = (trans) => {
+  let isDoingQuizIndicator = async (trans) => {
     let quizService = trans.injector().get('quizService');
 
-    // if (quizService.dangLamBai) {
-    //   let hoi = SweetAlertHelper.hoi("Bạn đang trong trạng thái làm bài, bạn muốn hủy bài làm?")
-    //     .then(
-    //       (result) => {
-    //         if (result.value) {
-    //           return trans.router.stateService.target(trans.to().name);
-    //         }
+    if (quizService.dangLamBai) {
+      let hoi = await SweetAlertHelper.hoi("Bạn đang trong trạng thái làm bài, bạn muốn hủy bài làm?")
+        .then(
+          (result) => {
+            if (result.value) {
+              return trans.router.stateService.target(trans.to().name);
+            }
 
-    //         return false;
-    //       }
-    //     );
+            return false;
+          }
+        );
 
-    //     console.log(hoi);
-    //     return hoi;
-    // }
+        if (hoi != false) {
+          return;
+        }
+        console.log(hoi);
+        return hoi;
+    }
     console.log('return');
     return;
   };
