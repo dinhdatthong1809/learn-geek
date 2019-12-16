f0b957e46ec55f9e5c4c022fb4f30e81e4e8d6d4
BlueShootingStar <BlueShootingStar1809@gmail.com>
2019-12-16 09:10:31 +0700

    [nothing]

--- app/modules/subject-module/subject-list/subject-list.component.js
+++ app/modules/subject-module/subject-list/subject-list.component.js
@@ -15,22 +15,26 @@ function controller() {
     this.pageCount = Math.ceil(this.size / this.numberOfItem);
 
     this.first = () => {
+        this.pageCount = Math.ceil(this.subjects.length / this.numberOfItem);
         this.begin = 0;
     };
 
     this.prev = () => {
+        this.pageCount = Math.ceil(this.subjects.length / this.numberOfItem);
         if (this.begin > 0) {
             this.begin -= this.numberOfItem;
         }
     };
 
     this.next = () => {
+        this.pageCount = Math.ceil(this.subjects.length / this.numberOfItem);
         if (this.begin < (this.pageCount - 1) * this.numberOfItem) {
             this.begin += this.numberOfItem;
         }
     }
 
     this.last = () => {
+        this.pageCount = Math.ceil(this.subjects.length / this.numberOfItem);
         this.begin = (this.pageCount - 1) * this.numberOfItem;
     }
 }
